0dfcafbb89dcafdb8670ee54d6be9c4b1c3118f1
David Unuigbe <mowems@gmail.com>
2025-02-26 19:19:11 +0200

    Included authenticateUser middleware to role.routes.ts & permission.routes.ts file

--- src/routes/permission.routes.ts
+++ src/routes/permission.routes.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import { createPermission, getPermissions, getPermissionById, updatePermission, deletePermission } from '../controllers/permission.controller';
-import { checkPermission } from '../middleware/authMiddleware';
+import { authenticateUser, checkPermission } from '../middleware/authMiddleware';
 
 const router = Router();
 
 // Permission Routes
-router.post('/', checkPermission(['WRITE_PERMISSION']), createPermission); // Create a new permission
-router.get('/', checkPermission(['READ_PERMISSION']), getPermissions); // Get all permissions
-router.get('/:id', checkPermission(['READ_PERMISSION']), getPermissionById); // Get a specific permission by id
-router.put('/:id', checkPermission(['WRITE_PERMISSION']), updatePermission); // Update permission details
-router.delete('/:id', checkPermission(['DELETE_PERMISSION']), deletePermission); // Delete a permission
+router.post('/', authenticateUser, checkPermission(['WRITE_PERMISSION']), createPermission); // Create a new permission
+router.get('/', authenticateUser, checkPermission(['READ_PERMISSION']), getPermissions); // Get all permissions
+router.get('/:id', authenticateUser, checkPermission(['READ_PERMISSION']), getPermissionById); // Get a specific permission by id
+router.put('/:id', authenticateUser, checkPermission(['WRITE_PERMISSION']), updatePermission); // Update permission details
+router.delete('/:id', authenticateUser, checkPermission(['DELETE_PERMISSION']), deletePermission); // Delete a permission
 
 export default router;
